4a56a423a7c78d5d1d1aebd583891a0d4bb64f0f
lyzirving <lyzirving@sina.com>
2023-01-13 16:01:05 +0800

    [ADD] add third party;
    Signed-off-by: lyzirving <lyzirving@sina.com>

--- LrWidget.h
+++ LrWidget.h
@@ -1,7 +1,11 @@
 #pragma once
 
 #include <QtWidgets/QWidget>
+#include <memory>
+
 #include "ui_LightRender.h"
+
+class Shader;
 
 class LrWidget : public QWidget
 {
@@ -11,6 +15,13 @@ public:
     LrWidget(QWidget *parent = nullptr);
     ~LrWidget();
 
+    virtual QPaintEngine* paintEngine() const override;
+
+protected:
+    virtual void closeEvent(QCloseEvent* e) override;
+
 private:
     Ui::LrWidgetClass ui;
+
+    std::shared_ptr<Shader> m_shader;
 };
