8231fc78679088439dc2d0ac1fff2c57452d9ade
lyzirving <lyzirving@sina.com>
2023-01-14 17:38:37 +0800

    [MOD] do initialization and release in main;
    Signed-off-by: lyzirving <lyzirving@sina.com>

--- main.cpp
+++ main.cpp
@@ -1,10 +1,33 @@
-#include "LrWidget.h"
 #include <QtWidgets/QApplication>
+
+#include "LrWidget.h"
+#include "Logger.h"
+
+void onInit()
+{
+    Logger::init();
+}
+
+void onQuit()
+{
+    Logger::release();
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    // do some initialization
+    onInit();
+
+    // show ui
     LrWidget w;
     w.show();
-    return a.exec();
+    int ret = a.exec();
+
+    // do some release
+    onQuit();
+
+    return ret;
 }
+
--- ui/LrWidget.cpp
+++ ui/LrWidget.cpp
@@ -1,11 +1,8 @@
 #include "LrWidget.h"
 #include "Shader.h"
-#include "Logger.h"
 
 LrWidget::LrWidget(QWidget *parent) : QWidget(parent, Qt::MSWindowsOwnDC)
 {
-    Logger::init();
-
     setAttribute(Qt::WA_PaintOnScreen);
     setAttribute(Qt::WA_NoSystemBackground);
     setAutoFillBackground(true);
@@ -22,5 +19,4 @@ QPaintEngine* LrWidget::paintEngine() const { return nullptr; }
 
 void LrWidget::closeEvent(QCloseEvent* e)
 {
-    Logger::release();
 }
